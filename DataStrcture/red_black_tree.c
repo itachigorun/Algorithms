a95a171b37ee77dded6e57997bc2398f09ab9ae7
itachigorun <itachigorun@gmail.com>
2017-07-24 22:06:28 +0800

    red_black_tree

---
+++ DataStrcture/red_black_tree.c
@@ -0,0 +1,16 @@
+//author: itachigorun
+//date: 2017-07-24
+
+LEFT_ROTATE(T, x)
+    y = x.right		// set y
+    x.right = y.left	// turn y's left subtree into x's right subtree
+    if y.left != T.nil
+    	y.left.p = x
+    y.p = x.p		// link x's parent to y
+    if x.p == T.nil
+    	T.root = y
+    elseif x == x.p.left
+    	x.p.left = y
+    else x.p.right = y
+    y.left = x		// put x on y's left
+    x.p = y
