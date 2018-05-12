ff8dae748b350450c59c27716e9b205824ce72c5
itachigorun <itachigorun@gmail.com>
2018-05-12 15:34:57 +0800

    带头指针尾指针的链表

--- DataStrcture/list_no_headnode.c
+++ DataStrcture/list_no_headnode.c
@@ -1,146 +1,126 @@
-// itachigorun 
-// 2017-04-04
-
-
 #include<stdio.h>
 #include<stdlib.h>
-#include<stdbool.h>
 
-typedef struct node{
-    int key;
-    struct node *prev;
-    struct node *next;
-}Node;
+typedef struct list {
+	int a;
+	char b;
+	struct list *next;
+	struct list *prev;
+}listnode;
 
-
-void InitList(Node **L)
+void listInit(listnode **Head, listnode **Tail)
 {
-    *L = NULL;      
-}    
-
-void InsertHeadList(Node **L, int input_num)
-{
-    Node *p = malloc(sizeof(Node));
-    if(!p)
-    {
-        printf("Memory error\n");
-        return ;
-    }
-    p->key = input_num;
-    p->next = *L;
-    if(*L)
-        (*L)->prev = p;
-    
-    *L = p;
-    p->prev = NULL;
+	*Head = NULL;
+	*Tail = NULL;
 }
 
-/* 无头结点链表尾巴插入节点,用二级指针操作第一个节点插入，若是带头节点链表则插入不用二级指针
-list* concat(list* l, list* r)
+bool listTailInsert(listnode **Head, listnode **Tail, int a, char b)
 {
-    list** indir = &l;
-    while(*indir) {
-        indir = &((*indir)->next);
-    }
-    *indir = r;
-    return l;
-}
-*/
+	listnode *temp = (listnode *)malloc(sizeof(listnode));
+	if (!temp)
+	{
+		printf("malloc failed\n");
+		return false;
+	}
+	temp->a = a;
+	temp->b = b;
+	temp->next = NULL;
+	temp->prev = (*Tail);
+	if (*Tail)
+	{
+		(*Tail)->next = temp;
+	}
+	else
+	{
+		(*Head) = temp;
+	}
+	
+	(*Tail) = temp;
 
-
-Node *ListSearch(Node *L, int input_num)
-{
-    Node *p = L;
-    while(p!= NULL && p->key!=input_num)
-        p = p->next;
-    
-    return p;
-}
-
-void DeleteList(Node *L, int input_num)
-{
-    Node *p = NULL;
-    if(L == NULL)
-    {
-        printf("The list is empty\n");
-	    return ;
-    }
-
-    p = ListSearch(L, input_num);
-    if(!p)
-    {
-        printf("The number is not found\n");
-        return ;
-    }
-    if(p->prev!=NULL)
-        p->prev->next = p->next;
-    else
-        L = p->next;
-    if(p->next!=NULL)
-        p->next->prev = p->prev;
-    
-    free(p);
+	return true;
 }
 
-void PrintList(Node *L)
+bool listHeadInsert(listnode **Head, listnode **Tail, int a, char b)
 {
-    Node *p = L;
-    while(p!=NULL)
-    {
-        printf("%d ", p->key);
-        p = p->next;
-    }
-    printf("\n");
+	listnode *temp = (listnode *)malloc(sizeof(listnode));
+	if (!temp)
+	{
+		printf("malloc failed\n");
+		return false;
+	}
+	temp->a = a;
+	temp->b = b;
+	temp->prev = NULL;
+	temp->next = (*Head);
+	if (*Head)
+	{
+		(*Head)->prev = temp;
+	}
+	else
+	{
+		(*Tail) = temp;
+	}
+	(*Head) = temp;
+	return true;
 }
 
-void ClearList(Node *L)
+void listPrint(listnode *Head)
 {
-  Node *p = L;
-  while(p)
-  {
-     L = L->next;
-     free(p);
-     p = L;
-  }
+	listnode *node = Head;
+	while (node)
+	{
+		printf("a:[%d] b:[%c] \n", node->a, node->b);
+		node = node->next;
+	}
+}
+
+void listDelete(listnode **Head, listnode **Tail, listnode *temp)
+{
+	if (*Head == *Tail)
+	{
+		free(temp);
+		(*Head) = NULL;
+		(*Tail) = NULL;
+	}
+	else if (temp->next == NULL)
+	{
+		temp->prev->next = NULL;
+		(*Tail) = temp->prev;
+		free(temp);
+	}
+	else if (temp->prev == NULL)
+	{
+		temp->next->prev = NULL;
+		(*Head) = temp->next;
+		free(temp);
+	}
+	else
+	{
+		listnode * node = temp;
+		temp->next->prev = temp->prev;
+		temp->prev->next = temp->next;
+		free(node);
+	}
+}
+
+void listDestroy(listnode **Head, listnode **Tail)
+{
+	while (*Head)
+	{
+		listDelete(Head,Tail,*Head);
+	}
 }
 
 int main()
 {
-    Node *L = NULL;  //L是头指针，指向第一个节点
-    int select_num;
-    int input_num;
-    bool select_flag = true;
+	struct list * Head;
+	struct list * Tail;
 
-    InitList(&L);
-    while(select_flag)
-    {
-        printf("Please input the select 1)insert node  2)delete node 3)show list 4)exit: ");
-        scanf("%d", &select_num);
-        if(select_num == 1 || select_num ==2)
-        {
-	        printf("Please input the number: ");
-	        scanf("%d", &input_num);
-    	}
+	listInit(&Head, &Tail);
+	listHeadInsert(&Head, &Tail, 2, 'a');
+	listTailInsert(&Head, &Tail, 3, 'b');
+	listPrint(Head);
+	listDestroy(&Head, &Tail);
 
-        switch(select_num)
-        {
-            case 1:
-	            InsertHeadList(&L, input_num);
-	            break;
-            case 2:
-    	        DeleteList(L, input_num);
-    	        break;
-    	    case 3:
-    	        PrintList(L);
-    	        break;
-	        case 4:
-	            select_flag = false;
-	            ClearList(L);
-                break;
-	        default:
-	            printf("Please input again\n");
-        }
-    }
-
-    return 0;
+	return 0;
 }
